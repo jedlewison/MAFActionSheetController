5beb2485725626bb71e542803eb35e7d9a36b4e7
Jed Lewison <jed@magicappfactory.com>
2015-01-26 23:20:04 -0800

    Documentation update

--- MAFActionSheetController/MAFActionSheetController.h
+++ MAFActionSheetController/MAFActionSheetController.h
@@ -39,7 +39,15 @@ typedef NS_ENUM(NSUInteger, MAFActionSheetItemHandlerTiming){
 };
 
 /**
- `MAFActionSheetController` is fucked
+ `MAFActionSheetController` manages action sheet-style view controllers on iOS 7+ using MAFOverlay to present as an overlay from a bar button item or a view.
+
+ - Similar to UIAlertController, but with full control over layout and works with iOS 7+.
+ - Default layout is similar to popover style on iPhone
+ - Add custom header and footer
+ - Supports vertical scrolling for long sheets
+ - Choose primary and detail text attributes.
+ - Supply a custom background view for each action item to completely customize appearance.
+ - Uses MAFOverlay to coordinate presentations
  */
 
 
